07ac9d18f30d93583b4551e29cdea17a892a8240
Roemer Naber <200611@student.glu.nl>
2021-09-29 11:43:28 +0200

    admin gebruikersnaam&wachtwoord inouts toegevoegd

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -1,1 +1,14 @@
-<?php echo "<h2>connected</h2>"; ?>
+<?php include ('../verbinden/db_connect.php');
+echo "<br>"?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Beheerder login</title>
+</head>
+<body>
+    <input type="text" placeholder="Gebruikersnaam">
+    <input type="password" placeholder="Wachtwoord">
+    <input type="submit">
+</body>
+</html>
