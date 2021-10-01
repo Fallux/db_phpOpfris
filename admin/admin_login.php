72de65cec7c8eca68d935666a97660bbae8d9828
Roemer Naber <200611@student.glu.nl>
2021-10-01 21:29:36 +0200

    nieuwe bug... admin_login.php toont niet met de html en css variabelen.... AHAAAHA

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -5,30 +5,13 @@ include_once ('../include/db_connect.php');
 
 if(isset($_SESSION['logged_in'])) {
     //display index
-} else {
+}else {
     //display login 
     if (isset($_POST['email'], $_POST['password'])) {
-        $email      = $_POST['email'];
-        $password   = $_POST['password'];
-
+        $email = $_POST['email'];
+        $password = $_POST['password'];
         if (empty($email) or empty($password)) {
             $error = 'je moet ALLE velden invullen';
-        }else{
-            $query = $pdo->prepare("SELECT * FROM bibliotheek_leden WHERE email = ? AND password = ?");
-
-            $query->bindValue(1, $email);
-            $query->bindValue(2, $password);
-
-            $query->execute();
-
-            $num = $query->rowCount();
-
-            if ($num == 1) {
-                # user entered correct details
-            } else {
-                # user entered false data
-                $error = 'U heeft de verkeerde gebruikersnaam of wachtwoord getypt';
-            }
         }
     }
     ?>
@@ -36,7 +19,8 @@ if(isset($_SESSION['logged_in'])) {
     <html lang="en">
     <head>
         <title>Beheerder login</title>
-        <link rel="stylesheet" href="../style_opfris.css">
+        <link rel="stylesheet" href="../style_opfris.css"/>
+        <link rel="shortcut icon" href="#"/>
     </head>
     <body>
         <div class="container">
@@ -51,12 +35,13 @@ if(isset($_SESSION['logged_in'])) {
 
             <form action="admin_login.php" method="post" autocomplete="off">
                 <input type="mail" name="email" placeholder="email" />
-                <input type="password" name="password" placeholder="password"/>
+                <input type="password" name="password" placeholder="password" />
                 <input type="submit" value="Login" />
             </form>
         </div>
     </body>
     </html>
+
     <?php
 }
 
